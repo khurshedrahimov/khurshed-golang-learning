e1cb35ab29efb4b10eeb1acd109158a84dc475b5
khurshedrahimov <khurshedrahimov777@gmail.com>
2023-03-05 22:05:04 +0500

    functions.go excecise from Tour of Go

---
+++ exported-names.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	//fmt.Println(math.pi) //will not run be cause pi is lower case,which means is not exported
+	fmt.Println(math.Pi)
+}
